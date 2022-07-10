1a0c3c7e92e49bb71b7a9b975dbe46587098492e
John Traas <j.j.h.traas@gmail.com>
2022-07-10 16:35:50 +0000

    add django_more_complex app

---
+++ django_more_complex/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoMoreComplexConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_more_complex'
---
+++ django_more_complex/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.HomeTopics.as_view(), name='home'),
+]
