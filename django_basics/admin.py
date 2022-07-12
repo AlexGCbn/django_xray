0b6ee16e678b96b969d35e53974da6854093aab6
John Traas <j.j.h.traas@gmail.com>
2022-07-12 20:30:13 +0000

    add SubTopic model and makemigrations/migrate changes

--- django_basics/admin.py
+++ django_basics/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Topic
+from .models import Topic, SubTopic
 
 
 @admin.register(Topic)
@@ -8,3 +8,9 @@ class TopicAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('subject',)}
     list_display = ('subject', 'description')
     summernote_fields = ('description')
+
+
+@admin.register(SubTopic)
+class SubTopicAdmin(SummernoteModelAdmin):
+    list_display = ('topic', 'heading', 'paragraph', 'image')
+    summernote_fields = ('paragraph')
